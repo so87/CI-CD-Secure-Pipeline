3a218e91c13a1e6ebbaf99aa1550ff39070582a4
so87 <so87@evansville.edu>
2018-08-22 16:38:08 -0500

    buid now installs and configures docker and installs sonarqube

--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-echo WARNING! Need docker and docker compose installed in order to work
+echo WARNING! Need docker and docker compose to work. This script will attempt to isntall and configure it if it doesnt exist. This is for Centos7
+
 read -p "Press enter to continue"
+
+
+echo "installing docker"
+yum install curl -y
+yum install docker -y
+systemctl start docker 
+systemctl status docker
+systemctl enable docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "installing git"
+yum install git -y
+
+echo "installing wget"
+yum install wget -y
 
 
 echo Creating the necessary directories for jenkins and sonarqube
@@ -23,6 +40,7 @@ mv postgres /docker/
 mv sonarqube /docker/
 mv jenkins /docker/
 cd /docker/
+wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227-linux.zip
 
 echo "Open firewall"
 firewall-cmd --zone=public --add-port=9000/tcp --permanent
